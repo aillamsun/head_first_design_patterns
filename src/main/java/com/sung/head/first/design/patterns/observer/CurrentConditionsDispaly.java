10fe8b5ef165e389f95ec2adcf971f1b58d70363
sungang <1120_sungang@gmail.com>
2016-12-25 17:20:28 +0800

    add observer模式

---
+++ src/main/java/com/sung/head/first/design/patterns/observer/CurrentConditionsDispaly.java
@@ -0,0 +1,7 @@
+package com.sung.head.first.design.patterns.observer;
+
+/**
+ * Created by sungang on 2016/12/25.
+ */
+public class CurrentConditionsDispaly {
+}
--- src/main/java/com/sung/head/first/design/patterns/suck.java
+++
@@ -1,7 +0,0 @@
-package com.sung.head.first.design.patterns;
-
-/**
- * Created by sungang on 2016/12/25.
- */
-public class suck {
-}

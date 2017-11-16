e7bdaf247196c1046de7860aa2ff0ab9df6dde52
孙刚 <1120sungang@gmail.com>
2017-11-16 21:42:13 +0800

    工厂方法

---
+++ src/main/java/com/sung/head/first/design/patterns/factory/method/o2/Car.java
@@ -0,0 +1,7 @@
+package com.sung.head.first.design.patterns.factory.method.o2;
+
+/**
+ * Created by sungang on 2017/11/16.
+ */
+public interface Car {
+}
---
+++ src/main/java/com/sung/head/first/design/patterns/factory/method/o2/Factory.java
@@ -0,0 +1,9 @@
+package com.sung.head.first.design.patterns.factory.method.o2;
+
+/**
+ * Created by sungang on 2017/11/16.
+ */
+public abstract class CarFactory {
+
+    Car createCar(){return null;};
+}

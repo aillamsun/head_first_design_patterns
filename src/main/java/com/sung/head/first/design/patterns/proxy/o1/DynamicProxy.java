b89c854a098c5868d257bc038d811375e9fa557b
孙刚 <1120sungang@gmail.com>
2017-11-19 14:43:05 +0800

    动态代理

---
+++ src/main/java/com/sung/head/first/design/patterns/proxy/o1/DynamicProxy.java
@@ -0,0 +1,7 @@
+package com.sung.head.first.design.patterns.proxy.o1;
+
+/**
+ * Created by sungang on 2017/11/19.
+ */
+public class DynamicProxy {
+}
---
+++ src/main/java/com/sung/head/first/design/patterns/proxy/o1/IGamePlayer.java
@@ -0,0 +1,10 @@
+package com.sung.head.first.design.patterns.proxy.o1;
+
+/**
+ *
+ * 
+ *
+ * Created by sungang on 2017/11/19.
+ */
+public class IGame {
+}

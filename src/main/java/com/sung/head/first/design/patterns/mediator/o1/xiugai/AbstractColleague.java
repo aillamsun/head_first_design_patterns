db1d5874906c96dca08989a86659fd439613cb9e
孙刚 <1120sungang@gmail.com>
2017-11-19 16:02:27 +0800

    设计模式

---
+++ src/main/java/com/sung/head/first/design/patterns/mediator/o1/xiugai/AbstractColleague.java
@@ -0,0 +1,7 @@
+package com.sung.head.first.design.patterns.mediator.o1.xiugai;
+
+/**
+ * Created by sungang on 2017/11/19.
+ */
+public class AbstractColleague {
+}
---
+++ src/main/java/com/sung/head/first/design/patterns/mediator/o1/xiugai/AbstractMediator.java
@@ -0,0 +1,16 @@
+package com.sung.head.first.design.patterns.mediator.o1;
+
+/**
+ * 抽象中介类
+ * Created by sungang on 2017/11/19.
+ */
+public abstract class AbstractMediator {
+
+    protected Stock stock;
+    protected Purchase purchase;
+    protected Sale sale;
+
+    public AbstractMediator(){
+        
+    }
+}

8dcd1922c3f200aa856928b7fa78d3e64d5d2dd5
孙刚 <1120sungang@gmail.com>
2018-04-02 17:09:03 +0800

    组合设计模式

--- src/main/java/com/sung/head/first/design/patterns/composite/Client.java
+++ src/main/java/com/sung/head/first/design/patterns/composite/Client.java
@@ -4,4 +4,39 @@ package com.sung.head.first.design.patterns.composite;
  * Created by sungang on 2018/4/2.
  */
 public class Client {
+
+
+    public static void main(String[] args) {
+        Employee CEO = new Employee("John", "CEO", 30000);
+
+        Employee headSales = new Employee("Robert", "Head Sales", 20000);
+
+        Employee headMarketing = new Employee("Michel", "Head Marketing", 20000);
+
+        Employee clerk1 = new Employee("Laura", "Marketing", 10000);
+        Employee clerk2 = new Employee("Bob", "Marketing", 10000);
+
+        Employee salesExecutive1 = new Employee("Richard", "Sales", 10000);
+        Employee salesExecutive2 = new Employee("Rob", "Sales", 10000);
+
+        CEO.add(headSales);
+        CEO.add(headMarketing);
+
+        headSales.add(salesExecutive1);
+        headSales.add(salesExecutive2);
+
+        headMarketing.add(clerk1);
+        headMarketing.add(clerk2);
+
+        //print all employees of the organization
+        System.out.println(CEO);
+
+        for (Employee headEmployee : CEO.getSubordinates()) {
+            System.out.println(headEmployee);
+
+            for (Employee employee : headEmployee.getSubordinates()) {
+                System.out.println(employee);
+            }
+        }
+    }
 }
--- src/main/java/com/sung/head/first/design/patterns/composite/Employee.java
+++ src/main/java/com/sung/head/first/design/patterns/composite/Employee.java
@@ -1,7 +1,42 @@
 package com.sung.head.first.design.patterns.composite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by sungang on 2018/4/2.
+ * 通常，这种类需要定义 add(node)、remove(node)、getChildren() 这些方法。
  */
 public class Employee {
+
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates; //下属
+
+
+    // constructor
+    public Employee(String name,String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+    @Override
+    public String toString(){
+        return ("Employee :[ Name : " + name + ", dept : " + dept + ", salary :" + salary+" ]");
+    }
 }

760262c5cef5b9f655750797957f03b611f43c82
孙刚 <1120sungang@gmail.com>
2018-04-02 17:08:43 +0800

    组合设计模式

---
+++ src/main/java/com/sung/head/first/design/patterns/composite/Client.java
@@ -0,0 +1,7 @@
+package com.sung.head.first.design.patterns.composite;
+
+/**
+ * Created by sungang on 2018/4/2.
+ */
+public class Client {
+}
---
+++ src/main/java/com/sung/head/first/design/patterns/flyweight/Circle.java
@@ -0,0 +1,7 @@
+package com.sung.head.first.design.patterns.flyweight;
+
+/**
+ * Created by sungang on 2018/4/2.
+ */
+public class Circle {
+}
